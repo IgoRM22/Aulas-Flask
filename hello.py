e3187fc98cac723d62a62f894daa9172e4e5b28e
IgoRM22 <i.ramos@aluno.ifsp.edu.br>
2024-02-29 21:24:37 -0300

    Teste 02

--- hello.py
+++ hello.py
@@ -27,3 +27,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.route("/user/Igor%20Ramos/PT3019284/IFSP")
+def user():
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: Igor_Ramos</h2><h2>Prontuário: PT3019284</h2><h2>Instituição: IFSP</h2><p><a href="/">Voltar</a></p>'
+
+@app.route("/contextorequisicao")
+def context():
+    user_agent = request.headers.get('User-Agent')
+    url = request.remote_addr
+    ip = request.host_url
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do computador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><p><a href="/">Voltar</a></p>'.format(user_agent, url, ip)
